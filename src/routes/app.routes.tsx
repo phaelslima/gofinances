e2b5d03839fdd8c48e095c4baf4187bf07e3553e
Raphael Lima <phaelslima@gmail.com>
2022-06-24 16:35:52 -0300

    add Resume screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Platform } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useTheme } from "styled-components";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
-import { Platform } from "react-native";
+import { Resume } from "../screens/Resume";
 
 export type AppRoutesParamList = {
   Listagem: undefined;
@@ -64,7 +65,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (
             ({ size, color }) => (
