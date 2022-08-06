1a4e106f75fdf31a61b6bb17d0961dcb91f26fb9
Raphael Lima <phaelslima@gmail.com>
2022-08-06 16:29:01 -0300

    feat: add test on screens

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button, Text, TextInput, View } from 'react-native'
+
+export function Profile(){
+  return (
+    <View>
+      <Text testID="text-title">Perfil</Text>
+
+      <TextInput
+        testID="input-name"
+        placeholder="Nome"
+        autoCorrect={false}
+        value="Raphael"
+      />
+
+      <TextInput
+        testID="input-surname"
+        placeholder="Sobrenome"
+        value="Lima"
+      />
+
+      <Button
+        title="Salvar"
+        onPress={() => {}}
+      />
+    </View>
+  )
+}
