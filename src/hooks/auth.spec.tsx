e65093c24ebe661c3021bf445a5c4ad66d979b5b
Raphael Lima <phaelslima@gmail.com>
2022-08-06 20:06:42 -0300

    feat: add test auth hook

---
+++ src/hooks/auth.spec.tsx
@@ -0,0 +1,61 @@
+import fetchMock from 'jest-fetch-mock'
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+import { renderHook, act } from '@testing-library/react-hooks'
+import { mocked } from 'ts-jest/utils'
+import { AuthProvider, useAuth } from './auth'
+import { startAsync } from 'expo-auth-session'
+
+fetchMock.enableMocks()
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
+jest.mock('expo-auth-session')
+
+describe('Auth Hook', () => {
+  beforeEach(async () => {
+    mockAsyncStorage.clear()
+  })
+
+  it('should be able to sign in with Google account existing',  async() => {
+    const googleMocked = mocked(startAsync as any)
+    googleMocked.mockReturnValueOnce({
+      type: 'success',
+      params: {
+        access_token: 'any_token'
+      }
+    })
+
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        id: 'any_id',
+        email: 'phaelslima@gmail.com',
+        name: 'Raphael',
+        photo: 'any_photo.png'
+      })
+    )
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    })
+
+    await act(async () => await result.current.signInWithGoogle())
+
+    expect(result.current.user.email).toBe('phaelslima@gmail.com')
+  })
+
+  it('user should not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(startAsync as any)
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel'
+    })
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    })
+
+    await act(async () => await result.current.signInWithGoogle())
+
+    expect(result.current.user).not.toHaveProperty('id')
+  })
+
+})
+
